324f104d8864b8fad18dc463f96a69376aebfa8f
adriendelagneau <delagneauadrien@yahoo.fr>
2024-01-02 07:36:45 +0100

    zu cart

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,8 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import localFont from 'next/font/local'
- 
-// Font files can be colocated inside of `pages`
+
 
 
 
